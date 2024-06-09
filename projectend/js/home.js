0ed1b0492d5df15bf586476d3b4d7d7268362e4c
Namnguyen02102 <153411044+Namnguyen02102@users.noreply.github.com>
2024-06-09 12:11:16 +0700

    project
    sun 9

---
+++ projectend/js/home.js
@@ -0,0 +1,39 @@
+dataspbanchay = [{
+    "name": "Bao Lì Xì Tết Hạnh Phúc Ước Nguyện",
+    "price": 88000,
+    "img": "https://quatet.monamedia.net/wp-content/uploads/2019/05/0e55420e49ef9f927e33e32adad44617.jpg",
+    "describe": "Mang đến niềm vui và ước nguyện trong dịp Tết đến với “Bao Lì Xì Tết Hạnh Phúc Ước Nguyện”. Với thông điệp ý nghĩa và thiết kế độc đáo, bao lì xì này sẽ truyền tải những lời chúc tốt đẹp và sự may mắn đến người nhận. Hãy chia sẻ niềm vui và lời chúc tốt đẹp trong dịp Tết đến với bao lì xì này!"
+},
+{
+    "name": "Hộp Quà Tết Vạn Phúc",
+    "price": 620000 ,
+    "img": "https://quatet.monamedia.net/wp-content/uploads/2019/05/KR20347.jpg",
+    "describe": "Mang đến niềm vui và ước nguyện trong dịp Tết đến với “Bao Lì Xì Tết Hạnh Phúc Ước Nguyện”. Với thông điệp ý nghĩa và thiết kế độc đáo, bao lì xì này sẽ truyền tải những lời chúc tốt đẹp và sự may mắn đến người nhận. Hãy chia sẻ niềm vui và lời chúc tốt đẹp trong dịp Tết đến với bao lì xì này!"
+},
+{
+    "name": "Bánh Kẹo Tết Hương Vị Xưa",
+    "price": 336000 ,
+    "img": "https://quatet.monamedia.net/wp-content/uploads/2019/05/hn69082-scaled-1.jpg",
+    "describe": "Mang đến niềm vui và ước nguyện trong dịp Tết đến với “Bao Lì Xì Tết Hạnh Phúc Ước Nguyện”. Với thông điệp ý nghĩa và thiết kế độc đáo, bao lì xì này sẽ truyền tải những lời chúc tốt đẹp và sự may mắn đến người nhận. Hãy chia sẻ niềm vui và lời chúc tốt đẹp trong dịp Tết đến với bao lì xì này!"
+},
+{
+    "name": "Bao Lì Xì Tết Hạnh Phúc Ước Nguyện",
+    "price": 88000,
+    "img": "https://quatet.monamedia.net/wp-content/uploads/2019/05/0e55420e49ef9f927e33e32adad44617.jpg",
+    "describe": "Mang đến niềm vui và ước nguyện trong dịp Tết đến với “Bao Lì Xì Tết Hạnh Phúc Ước Nguyện”. Với thông điệp ý nghĩa và thiết kế độc đáo, bao lì xì này sẽ truyền tải những lời chúc tốt đẹp và sự may mắn đến người nhận. Hãy chia sẻ niềm vui và lời chúc tốt đẹp trong dịp Tết đến với bao lì xì này!"
+},
+{
+    "name": "Bao Lì Xì Tết Hạnh Phúc Ước Nguyện",
+    "price": 88000,
+    "img": "https://quatet.monamedia.net/wp-content/uploads/2019/05/0e55420e49ef9f927e33e32adad44617.jpg",
+    "describe": "Mang đến niềm vui và ước nguyện trong dịp Tết đến với “Bao Lì Xì Tết Hạnh Phúc Ước Nguyện”. Với thông điệp ý nghĩa và thiết kế độc đáo, bao lì xì này sẽ truyền tải những lời chúc tốt đẹp và sự may mắn đến người nhận. Hãy chia sẻ niềm vui và lời chúc tốt đẹp trong dịp Tết đến với bao lì xì này!"
+},
+{
+    "name": "Bao Lì Xì Tết Hạnh Phúc Ước Nguyện",
+    "price": 88000,
+    "img": "https://quatet.monamedia.net/wp-content/uploads/2019/05/0e55420e49ef9f927e33e32adad44617.jpg",
+    "describe": "Mang đến niềm vui và ước nguyện trong dịp Tết đến với “Bao Lì Xì Tết Hạnh Phúc Ước Nguyện”. Với thông điệp ý nghĩa và thiết kế độc đáo, bao lì xì này sẽ truyền tải những lời chúc tốt đẹp và sự may mắn đến người nhận. Hãy chia sẻ niềm vui và lời chúc tốt đẹp trong dịp Tết đến với bao lì xì này!"
+},
+
+]
+
